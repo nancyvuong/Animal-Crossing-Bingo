a5f5874575990b3061043eabce333c533dd733b5
Nancy Vuong <32500403+nancyvuong@users.noreply.github.com>
2021-06-22 13:48:04 -0700

    added functionality to the new game button

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 //extra randomized which repopulates squares and also
 //TEST CASE: make sure "" does not get into selected
 
-var lines; //contains the loaded villagers.txt file
 
 document.addEventListener("DOMContentLoaded", 
     function(event){
         fetch('data/villagers.txt')
         .then(response => response.text())
         .then((data) => {
-            lines = data.split('\n');
+            var lines = data.split('\n');
             for(var line = 0; line < 9; line++){
                 var squareid = "p" + line; 
                 //selected.push(lines[Math.floor(Math.random() * lines.length)]);
@@ -21,14 +20,30 @@ document.addEventListener("DOMContentLoaded",
 );
 
 function marked(squareid){
-    document.getElementById(squareid).style.backgroundColor='black';
+    if(document.getElementById(squareid).style.backgroundColor=='yellow'){
+        document.getElementById(squareid).style.backgroundColor='black';
+    }
+    
+    else{
+    document.getElementById(squareid).style.backgroundColor='yellow';
+    }
 }
 
 
 function filled(){
-    for(var line = 0; line < 9; line++){
-        var squareid = "p" + line; 
-        //selected.push(lines[Math.floor(Math.random() * lines.length)]);
-        document.getElementById(squareid).innerHTML = lines[Math.floor(Math.random() * lines.length)];
-      }
+    for (var i = 0; i < 9; i++){
+        var squareid = "p"+i;
+        document.getElementById(squareid).style.backgroundColor='yellow';
+    }
+
+    fetch('data/villagers.txt')
+    .then(response => response.text())
+    .then((data) => {
+        var lines = data.split('\n');
+        for(var line = 0; line < 9; line++){
+            var squareid = "p" + line; 
+            //selected.push(lines[Math.floor(Math.random() * lines.length)]);
+            document.getElementById(squareid).innerHTML = lines[Math.floor(Math.random() * lines.length)];
+        }
+    })
 }
